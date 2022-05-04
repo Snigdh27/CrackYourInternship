50ff506608b2c126a2f6a66393f21387721f3886
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2022-05-04 11:29:56 +0530

    Runtime: 8 ms Memory Usage: 9.7 MB

---
+++ 226InvertBinaryTree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        
+        TreeNode* temp=root->left;
+        root->left=invertTree(root->right);
+        root->right=invertTree(temp);
+        
+        return root;
+    }
+};
